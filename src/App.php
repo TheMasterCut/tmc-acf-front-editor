70b807bbab8bbb7cfc6bf9d2a588a7d772bd89b8
themastercut <hello@themastercut.co>
2018-06-25 11:02:31 +0200

    Add: Project setup.

---
+++ src/App.php
@@ -0,0 +1,22 @@
+<?php
+namespace tmc\acffe\src;
+
+/**
+ * @author jakubkuranda@gmail.com
+ * Date: 25.06.2018
+ * Time: 11:01
+ */
+
+use shellpress\v1_2_5\ShellPress;
+
+class App extends ShellPress {
+
+	/**
+	 * Called automatically after core is ready.
+	 *
+	 * @return void
+	 */
+	protected function onSetUp() {
+		// TODO: Implement onSetUp() method.
+	}
+}
